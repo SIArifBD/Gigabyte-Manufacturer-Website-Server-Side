19e5ea95c05df04e5e98a88d49a1cd9183a56de6
SIArifBD <si.arif.cse@gmail.com>
2022-05-27 22:09:28 +0600

    11th commit

--- index.js
+++ index.js
@@ -144,12 +144,12 @@ async function run() {
             res.send(result);
         });
         //delete product
-        // app.delete('/product/:id', verifyJWT, verifyAdmin, async (req, res) => {
-        //     const id = req.params.id;
-        //     const filter = { _id: ObjectId(id) };
-        //     const result = await productCollection.deleteOne(filter);
-        //     res.send(result);
-        // });
+        app.delete('/product/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(filter);
+            res.send(result);
+        });
         //order post api
         app.post('/order', verifyJWT, async (req, res) => {
             const order = req.body;
